f2008f59a5f376e1a634023e7a6ea64625896d9c
fabiansig <fabian.sigmund1@gmail.com>
2023-07-23 19:37:14 +0200

    Einstufungstest

--- src/_0000_Einstufungstest/E/Analytics.java
+++ src/_0000_Einstufungstest/E/Analytics.java
@@ -1,10 +1,15 @@
 package _0000_Einstufungstest.E;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-class Analytics {
+public class Analytics {
 
-    public List<Person> containsBindestrich(List<Person> personen){
-        return personen.stream ().filter ()
+    public static List<Person> containsBindestrich(List<Person> personen){
+        return personen.stream ().filter (person -> person.getNachname ().contains ("-")).collect(Collectors.toList());
+    }
+
+    public static int nHinterlegteAdressen(List<Person> personen, int n){
+        return (int) personen.stream ().filter (person -> person.getAdressen ().size () >= n).count ();
     }
 }
--- src/_0000_Einstufungstest/E/Main.java
+++ src/_0000_Einstufungstest/E/Main.java
@@ -1,15 +1,22 @@
 package _0000_Einstufungstest.E;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import static _0000_Einstufungstest.D.DateiErsteller.erstelleDatei;
+import static _0000_Einstufungstest.E.Analytics.containsBindestrich;
+import static _0000_Einstufungstest.E.Analytics.nHinterlegteAdressen;
+import static _0000_Einstufungstest.E.DateiErsteller.erstelleDatei;
 
 class Main {
 
     public static void main(String[] args) {
 
-        Person p1 = new Person ("Herr", "Donald", "Duck", LocalDate.parse("2000-01-08"));
+
+        Person p1 = new Person ("Herr", "Donald", "Duck-Duck", LocalDate.parse("2000-01-08"));
         Person p2 = new Person ("Herr", "Rober", "Duck", LocalDate.parse("2001-01-08"));
+        List<Person> personen = Arrays.asList (p1, p2);
         ElektronischeAdresse mail1 = new ElektronischeAdresse ("Donald@gmail.com");
         ElektronischeAdresse mail2 = new ElektronischeAdresse ("Donald2@gmail.com");
         PostalischeAdresse a1 = new PostalischeAdresse ("Grubenweg", 4, "52323", "LazyTown");
@@ -25,6 +32,8 @@ class Main {
         System.out.println (p2);
 
         erstelleDatei(p1, p2);
+        System.out.println (nHinterlegteAdressen(personen, 2));
+        System.out.println (containsBindestrich (personen));
 
     }
 }
